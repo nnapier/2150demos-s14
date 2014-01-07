f20e1f9beb299d656114a0b9be4088557b05c635
nnapier <nnapier@ggc.edu>
2014-01-07 10:51:24 -0500

    Adding another class

---
+++ chapter8/src/January8Class.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+
+/**
+ * @author nnapier
+ *
+ */
+public class January8Class 
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+		// Another simple class that doesn't do anything
+		// Basically, testing out my GitHub account is setup 
+		// correctly on the computer
+		System.out.println("Hello there!");
+
+	}
+
+}
