4dab3e6e5dfd5bebdf30eb58196e797e19a35fc1
Nannette Napier <nnapier@ggc.edu>
2014-01-07 10:40:07 -0500

    Adding a class

---
+++ chapter8/src/WelcomeClass.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+
+/**
+ * @author nnapier
+ *
+ */
+public class WelcomeClass 
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+		System.out.println("Welcome to GGC!");
+
+	}
+
+}
