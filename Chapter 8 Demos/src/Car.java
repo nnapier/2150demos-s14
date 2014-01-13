567e902685527ca605e06972eb32b20051f149d4
nnapier <nnapier@ggc.edu>
2014-01-13 10:52:35 -0500

    Jan 13 changes

---
+++ Chapter 8 Demos/src/Car.java
@@ -0,0 +1,15 @@
+/**Class: Car
+ * @author Nannette Napier
+ * @version 1.0
+ * Course : ITEC 2150 Spring 2014
+ * Written: Jan 13, 2014
+ *
+ * This class - TODO
+ *
+ * Purpose: - TODO
+ */
+
+public class Car
+{
+
+}
